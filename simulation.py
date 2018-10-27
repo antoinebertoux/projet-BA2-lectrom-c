65f0e98bdf8fff83f9480aaf2b4365dc1b39be84
RaphLins <raphael.secondaire@gmail.com>
2018-10-27 16:37:12 +0200

    clean up

--- simulation.py
+++ simulation.py
@@ -1,110 +1,153 @@
-import turtle
+import pygame
+import sys
 import random
-import time
+import math
+
+white = (255,255,255)
+black = (0,0,0)
+blue = (0,0,255)
+green = (0, 255, 0)
+red = (255, 0, 0)
 
 class Tube:
-    def __init__(self, position, diameter):
-        self.turt = turtle.Turtle()
+    def __init__(self, position, radius):
         self.position = position
-        self.diameter = diameter
-        self.turt.hideturtle()  
-        self.turt.color('blue')
-        self.draw()
-    def draw(self):
-        self.turt.clear()
-        drawCircle(self.position, self.diameter, self.turt)
-    def setPosition(self, pos):
+        self.radius = radius
+    def draw(self, scr):
+        pygame.draw.circle(scr, blue, self.position, self.radius, 0)
+    def set_pos(self, pos):
         self.position = pos
-        self.draw()
 
 class Car:
-    def __init__(self, position, diameter):
-        self.turt = turtle.Turtle()
-        self.position = position
-        self.diameter = diameter
-        self.turt.hideturtle()
-        self.turt.color('red')
-        self.draw()
-    def draw(self):
-        self.turt.clear()
-        drawCircle(self.position, self.diameter, self.turt)
-    def setPosition(self, pos):
+    def __init__(self, background):
+        self.position = pygame.math.Vector2(0,0)
+        self.angle = 0
+        self.car_img = pygame.image.load('car.png').convert_alpha()
+        #vector from center of image to center of rotation
+        self.offset = pygame.math.Vector2(-60, 0)
+        #vector from center of rotation to sensor
+        self.sensor_offset = pygame.math.Vector2(-30, 0)
+        self.background = background
+        self.sensors_values = [False, False, False]
+    def draw(self, scr):
+        ###draw image
+        offset_rotated = self.offset.rotate(-self.angle)
+        rotated_image = pygame.transform.rotate(self.car_img, self.angle)
+        rect = rotated_image.get_rect()
+        rect.center = self.position + offset_rotated
+        scr.blit(rotated_image, rect)
+
+        ###draw center of rotation
+        pygame.draw.circle(scr, white, self.position, 2, 0)
+
+        ###draw sensors
+        sensor_offset_rotated = self.offset.rotate(-self.angle)
+        sensor_offset_rotated_perpendicular = sensor_offset_rotated.rotate(90)
+        sensors_center = self.position + sensor_offset_rotated
+        n = len(self.sensors_values)
+        for i in range(n):
+            sensor_pos = sensors_center + (i-n//2)*sensor_offset_rotated_perpendicular*0.3
+            sensor_pos = (int(sensor_pos[0]), int(sensor_pos[1]))
+            if 0 < sensor_pos[0] < 1500 and 0 < sensor_pos[1] < 800:
+                sensor_value = (background.get_at(sensor_pos) == black)
+            else:
+                sensor_value = False
+            self.sensors_values[i] = sensor_value
+            if sensor_value:
+                color = red
+            else:
+                color = green
+            pygame.draw.circle(scr, color, sensor_pos, 5, 0)
+    def set_pos(self, pos):
         self.position = pos
-        self.draw()
+    def set_angle(self, ang):
+        self.angle = angle
+    def get_sensors_values(self):
+        return self.sensors_values
 
-def drawCircle(position, diameter, t):
-    t.penup()
-    t.setheading(0)
-    t.setpos(position)
-    t.pendown()
-    t.begin_fill()
-    t.circle(diameter)
-    t.end_fill()
-    t.penup()
+def get_background():
+    background = pygame.Surface((1500,800))
+    background.fill(white)
+    pygame.draw.rect(background, black, (50,50,1400,700), 16)
+    pygame.draw.line(background, black, (285,50), (285,750), 16)
+    pygame.draw.line(background, black, (50,400), (285,400), 16)
+    tube_point_list = []
+    x = 360
+    y = 160
+    for i in range(6):
+        pygame.draw.line(background, black, (x,y), (x,y+470), 16)
+        tube_point_list.append((x,y+110))
+        tube_point_list.append((x,y+360))
+        if i%2==1 and i<5:
+            pygame.draw.line(background, black, (x,y), (x+200,y), 16)
+        elif i<5:
+            pygame.draw.line(background, black, (x,y+470), (x+200,y+470), 16)
+        x+=200
+    return background, tube_point_list
 
-def drawScotch():
-    t.setpos(0,0)
-    t.setheading(-90)
-    tube_position_list =[]
-    t.pensize(1.6)
-    for i in range(11):
-        if i % 2 == 0:
-            t.forward(20)
-            tube_position_list.append(t.position())
-            t.forward(50)
-            tube_position_list.append(t.position())
-            t.forward(20)
-        else:
-            t.forward(40)
-        if i//2 % 2 == 0:
-            t.left(90)
-        else:
-            t.right(90)
-    t.penup()
-    t.pensize(1)
-    return tube_position_list
+screen = pygame.display.set_mode((1500,800))
+pygame.init()
+pygame.display.set_caption('Simulation groupe 10')
+clock = pygame.time.Clock()
 
-def drawTubes(tube_position_list):
-    lst = tube_position_list
-    size_list = [3.2,3.2,4,4,5,5]
-    tube_list = []
-    for diameter in size_list:
-        point = random.choice(lst)
-        tube_list.append(Tube(point, diameter))
-        lst.remove(point)
-    return tube_list
-    
-def drawFrame():
-    t.setpos(-58,19)
-    t.setheading(0)
-    t.pendown()
-    t.forward(265)
-    t.right(90)
-    t.forward(132)
-    t.right(90)
-    t.forward(265)
-    t.right(90)
-    t.forward(132)
-    t.right(90)
-    t.penup()
-    t.setpos(0,0)
+background, tube_point_list = get_background()
+tube_point_list = random.sample(tube_point_list, 6)
+tube_radius_list = [16,16,20,20,25,25]
+tube_list = []
+for i in range(6):
+    tube_list.append(Tube(tube_point_list[i], tube_radius_list[i]))
 
+x =  (360)
+y = (160)
 
-turtle.setup(500,500)
-t = turtle.Turtle()
-t.speed(3)
-t.color('black')
-t.hideturtle()  
-turtle.tracer(0, 0)
-tube_position_list = drawScotch()
-drawFrame()
-tube_list = drawTubes(tube_position_list)
-car = Car((0,0),10)
-x =0
+tx =  (360)
+ty = (160)
+
+x_change = 0
+y_change = 0
+car_speed = 0
+angle_change = 0
+angle = 270
+car = Car(background)
+
+speed = 0
+
 while True:
-    car.setPosition((x,100))
-    x+=3
-    time.sleep(.1)
-    turtle.update()
-    
-turtle.done()
+    ###update position
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                 pygame.quit();
+                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    angle_change = 1
+                elif event.key == pygame.K_RIGHT:
+                    angle_change = -1
+                elif event.key == pygame.K_UP:
+                    speed = 4
+                elif event.key == pygame.K_DOWN:
+                    speed = -4
+                elif event.key == pygame.K_q:
+                    pygame.quit();
+                    sys.exit()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT :
+                    angle_change = 0
+                elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    speed = 0
+
+    angle += angle_change
+    angle = angle%360
+
+    x += int(speed * math.cos(math.radians(angle)))
+    y += int(- speed * math.sin(math.radians(angle)))
+
+    car.set_pos((x,y))
+    car.set_angle(angle)
+
+    ###draw everything
+    screen.blit(background, (0,0))
+    for tube in tube_list:
+        tube.draw(screen)
+    car.draw(screen)
+    pygame.display.update()

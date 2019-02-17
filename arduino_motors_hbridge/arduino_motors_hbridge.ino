d44bcbdcba7dc7965c7803ea494a5955ac1affee
RaphLins </>
2019-02-17 14:03:04 +0100

    update

--- arduino_motors_hbridge/arduino_motors_hbridge.ino
+++ arduino_motors_hbridge/arduino_motors_hbridge.ino
@@ -23,7 +23,7 @@ volatile long left_count = 0;
 volatile long right_count = 0;
 
 //breakbeam
-#define BREAKBEAM_PIN 12
+#define BREAKBEAM_PIN 13
 
 //line sensors
 #define LINE_PIN_L A0
@@ -31,27 +31,28 @@ volatile long right_count = 0;
 #define LINE_PIN_R A2
 
 //PID
-#define KP_L .3
+#define KP_L .25
 #define KI_L .0003
 #define KD_L 300
-#define KP_R .12
+#define KP_R .25
 #define KI_R .0003
-#define KD_R 0
+#define KD_R 300
 double true_speed_left, speed_left, tension_left, true_speed_right, speed_right, tension_right, delta_tension_right, delta_tension_left;
 AutoPID myPID_L(&true_speed_left, &speed_left, &delta_tension_left, -255, 255, KP_L, KI_L, KD_L);
 AutoPID myPID_R(&true_speed_right, &speed_right, &delta_tension_right, -255, 255, KP_R, KI_R, KD_R);
 unsigned long last_measure_update;
 
 //main code
-long DELTA_T=500; // en ms
-long WHEEL_RADIUS= 60; //en mm
-#define NORMAL_SPEED 200 //vitesses en mm/s
-#define SLOW_SPEED 40
-#define TURNING_SPEED 80
-long distance_step;
-long distance_since_last_turn;
-long start_time = millis();
-int sensor_left,sensor_middle,sensor_right;
+double distance_step_left, distance_step_right, distance_step, distance_since_last_turn;
+int DELTA_T=500; // en ms
+int WHEEL_RADIUS= 60; //en mm
+int NORMAL_SPEED  = 250; //vitesses en mm/s
+int SLOW_SPEED  = 150;
+int TURNING_SPEED = 150;
+unsigned long start_time = millis();
+unsigned long current_time = millis();
+int sensor_left,sensor_middle,sensor_right, breakbeam;
+
 void setup(){
   //serial
   Serial.begin(9600);
@@ -69,13 +70,12 @@ void setup(){
   pinMode(ENCODER_PIN_B_L,INPUT);
   pinMode(ENCODER_PIN_A_R,INPUT);
   pinMode(ENCODER_PIN_B_R,INPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A_L),count_left,FALLING);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A_R),count_right,FALLING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A_L),increment_left_count,FALLING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A_R),increment_right_count,FALLING);
 
   //Breakbeam
-  pinMode(BREAKBEAM_PIN, INPUT);
-  digitalWrite(BREAKBEAM_PIN, HIGH);
-
+  pinMode(BREAKBEAM_PIN, INPUT_PULLUP);
+  
   //line sensors
   pinMode(LINE_PIN_L, INPUT);
   pinMode(LINE_PIN_M, INPUT);
@@ -88,60 +88,64 @@ void setup(){
 
 
 void loop(){
-  //debug
-  //Serial.println(left_count, right_count);
-  Serial.print(true_speed_left);
-  Serial.print(" ");
-  Serial.println(true_speed_right);
-  //Serial.println(digitalRead(BREAKBEAM_PIN));
+  //sensors values
+  breakbeam = digitalRead(BREAKBEAM_PIN);
+  sensor_left = analogRead(LINE_PIN_L);
+  sensor_middle = analogRead(LINE_PIN_M);
+  sensor_right = analogRead(LINE_PIN_R);
+
+  current_time = millis();
   
-   //sensor_left = analogRead(LINE_PIN_L);
-   //sensor_middle = analogRead(LINE_PIN_M);
-   //sensor_right = analogRead(LINE_PIN_R);
-  //Serial.println(analogRead(LINE_PIN_L));
   //main code
   //speed_left = NORMAL_SPEED*sin((millis()-start_time)/10000.0);
-  speed_left = 100;
-  speed_right = 100;
+  speed_left = SLOW_SPEED;
+  speed_right = SLOW_SPEED;
   
   //motors PID
-  update_measured_speeds();
+  if ((current_time - last_measure_update) > DELTA_T) {
+    update_measures(DELTA_T);
+    last_measure_update = current_time;
+    
+    //debug
+    //Serial.println(left_count, right_count);
+    Serial.print(true_speed_left);
+    Serial.print(" ");
+    Serial.println(true_speed_right);
+  }
   myPID_L.run();
   myPID_R.run();
   update_motors_tension();
-  
-  delay(300);
+  delay(300)
 }
 
 
-void update_measured_speeds(){ // permet de connaitre la vitesse de rotation des moteurs
-  if ((millis() - last_measure_update) > DELTA_T) {
-    true_speed_left=WHEEL_RADIUS*6283*left_count/(2940*DELTA_T);//6283=2*pi*1000 //2940=7*2*210=pulses par rotation
-    true_speed_right=WHEEL_RADIUS*6283*right_count/(2940*DELTA_T);
-    left_count=0;
-    right_count=0;
-    last_measure_update = millis();
-    distance_step = (true_speed_left+true_speed_right)*DELTA_T/(2*1000); // mesure la distance parcourue pendant delta t
-    distance_since_last_turn += distance_step; // mesure la distance total parcourue
-  }
+void update_measures(unsigned long delta_t){ // permet de connaitre la vitesse de rotation des moteurs
+  distance_step_left = WHEEL_RADIUS*2*PI*left_count/2940; //2940=7*2*210=pulses par rotation
+  distance_step_right = WHEEL_RADIUS*2*PI*right_count/2940; 
+  true_speed_left=distance_step_left*1000/delta_t;
+  true_speed_right=distance_step_right*1000/delta_t;
+  left_count=0;
+  right_count=0;
+  distance_step = (distance_step_left+distance_step_right)/2; // mesure la distance parcourue pendant delta t
+  distance_since_last_turn += distance_step; // mesure la distance total parcourue
 }
 
-void count_left() {
+void increment_left_count() {
   if (digitalRead(ENCODER_PIN_A_L) != digitalRead(ENCODER_PIN_B_L))left_count--; // permet de connaitre le sens de rotation du moteur gauche
   else left_count++;
 }
 
-void count_right() {
-  if (digitalRead(ENCODER_PIN_A_R) != digitalRead(ENCODER_PIN_B_R))right_count++; // permet de connaitre le sens de rotation du moteur droit
-  else right_count--;
+void increment_right_count() {
+  if (digitalRead(ENCODER_PIN_A_R) != digitalRead(ENCODER_PIN_B_R))right_count--; // permet de connaitre le sens de rotation du moteur droit
+  else right_count++;
 }
 
 void update_motors_tension(){
-  tension_left = update_tension(tension_left+delta_tension_left, 1);
+  tension_left = update_tension(tension_left+delta_tension_left, 1); //delta_tension_left et right sont donnés par le PID
   tension_right = update_tension(tension_right+delta_tension_right, 0);
 }
 
-double update_tension(int tension, int motor){
+double update_tension(int tension, int motor //1 for left, 0 for right){
   if (tension>255)tension = 255;
   if (tension<-255)tension = -255;
   boolean inPin1 = LOW;
@@ -153,11 +157,11 @@ double update_tension(int tension, int motor){
   if(motor == 1){
   digitalWrite(LIN1, inPin1);
   digitalWrite(LIN2, inPin2);
-  analogWrite(PWML, abs(100));
+  analogWrite(PWML, abs(tension));
   }else{
   digitalWrite(RIN1, inPin1);
   digitalWrite(RIN2, inPin2);
-  analogWrite(PWMR, abs(100));
+  analogWrite(PWMR, abs(tension));
   }
   return tension;
 }

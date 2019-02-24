8fbce03c53e03489d5c0a873204a686acc1af3a4
RaphLins </>
2019-02-24 14:00:41 +0100

    follow line

--- arduino_motors_hbridge/arduino_motors_hbridge.ino
+++ arduino_motors_hbridge/arduino_motors_hbridge.ino
@@ -21,9 +21,11 @@
 #define ENCODER_PIN_B_R 5
 volatile long left_count = 0;
 volatile long right_count = 0;
+long last_left_count = 0;
+long last_right_count = 0;
 
 //breakbeam
-#define BREAKBEAM_PIN 13
+#define BREAKBEAM_PIN 6
 
 //line sensors
 #define LINE_PIN_L A0
@@ -31,27 +33,33 @@ volatile long right_count = 0;
 #define LINE_PIN_R A2
 
 //PID
-#define KP_L .25
-#define KI_L .0003
-#define KD_L 300
-#define KP_R .25
-#define KI_R .0003
-#define KD_R 300
-double true_speed_left, speed_left, tension_left, true_speed_right, speed_right, tension_right, delta_tension_right, delta_tension_left;
-AutoPID myPID_L(&true_speed_left, &speed_left, &delta_tension_left, -255, 255, KP_L, KI_L, KD_L);
-AutoPID myPID_R(&true_speed_right, &speed_right, &delta_tension_right, -255, 255, KP_R, KI_R, KD_R);
+#define KP_L 0
+#define KI_L 0
+#define KD_L 0
+#define KP_R 0
+#define KI_R 0
+#define KD_R 0
+double true_speed_left, speed_left, true_speed_right, speed_right, delta_coef_right, delta_coef_left;
+double coef_left = 1;
+double coef_right = 1;
+AutoPID myPID_L(&true_speed_left, &speed_left, &delta_coef_left, -255, 255, KP_L, KI_L, KD_L);
+AutoPID myPID_R(&true_speed_right, &speed_right, &delta_coef_right, -255, 255, KP_R, KI_R, KD_R);
 unsigned long last_measure_update;
 
 //main code
-double distance_step_left, distance_step_right, distance_step, distance_since_last_turn;
-int DELTA_T=500; // en ms
-int WHEEL_RADIUS= 60; //en mm
-int NORMAL_SPEED  = 250; //vitesses en mm/s
-int SLOW_SPEED  = 150;
-int TURNING_SPEED = 150;
-unsigned long start_time = millis();
+const int DELTA_T=100; // en ms
+const int WHEEL_RADIUS= 60; //en mm
+const int NORMAL_SPEED  = 250; //vitesses en mm/s
+const int SLOW_SPEED  = 150;
+const int TURNING_SPEED = 150;
+const int TURN_AFTER_COUNT_1 = 6000*2;
+const int TURN_AFTER_COUNT_2 = 2200*2;
+const int LINE_SENSOR_TOLERANCE = 200;
+const int K = 5;
 unsigned long current_time = millis();
 int sensor_left,sensor_middle,sensor_right, breakbeam;
+int last_breakbeam = 1;
+int tube_enter_count = 0;
 
 void setup(){
   //serial
@@ -74,7 +82,8 @@ void setup(){
   attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_A_R),increment_right_count,FALLING);
 
   //Breakbeam
-  pinMode(BREAKBEAM_PIN, INPUT_PULLUP);
+  pinMode(BREAKBEAM_PIN, INPUT);
+  digitalWrite(BREAKBEAM_PIN, HIGH);
   
   //line sensors
   pinMode(LINE_PIN_L, INPUT);
@@ -88,46 +97,74 @@ void setup(){
 
 
 void loop(){
-  //sensors values
+  //SENSORS VALUES//
   breakbeam = digitalRead(BREAKBEAM_PIN);
-  sensor_left = analogRead(LINE_PIN_L);
-  sensor_middle = analogRead(LINE_PIN_M);
-  sensor_right = analogRead(LINE_PIN_R);
-
+  sensor_left = analogRead(LINE_PIN_L) > LINE_SENSOR_TOLERANCE;
+  sensor_middle = analogRead(LINE_PIN_M) > LINE_SENSOR_TOLERANCE;
+  sensor_right = analogRead(LINE_PIN_R) > LINE_SENSOR_TOLERANCE;
   current_time = millis();
   
-  //main code
-  //speed_left = NORMAL_SPEED*sin((millis()-start_time)/10000.0);
-  speed_left = SLOW_SPEED;
-  speed_right = SLOW_SPEED;
-  
-  //motors PID
-  if ((current_time - last_measure_update) > DELTA_T) {
-    update_measures(DELTA_T);
-    last_measure_update = current_time;
-    
-    //debug
-    //Serial.println(left_count, right_count);
-    Serial.print(true_speed_left);
-    Serial.print(" ");
-    Serial.println(true_speed_right);
+  //MAIN CODE//
+  if(sensor_left == sensor_right){
+    speed_left = 200;
+    speed_right = 200;
   }
-  myPID_L.run();
-  myPID_R.run();
+  else if(sensor_left){
+    speed_left = 150;
+    speed_right = 200;
+  }
+  else if(sensor_right){
+    speed_left = 200;
+    speed_right = 150;
+  }
+  
+  //double error = (sensor_left + sensor_middle*10 + sensor_right*20)/double( sensor_left + sensor_middle + sensor_right) - 10;
+  //Serial.println(error);
+
+  
+  if (!breakbeam && last_breakbeam) {//tube enter
+    tube_enter_count = left_count+right_count;
+  }
+  if (breakbeam && !last_breakbeam) {//tube exit
+    double diameter = get_distance(left_count+right_count-tube_enter_count)/2;
+    Serial.println(diameter);
+  }
+  last_breakbeam=breakbeam;
+  
+  //MOTORS PID//
+  if ((current_time - last_measure_update) > DELTA_T) {
+    update_measured_speeds(current_time - last_measure_update);
+    last_measure_update = current_time;
+    myPID_L.run();
+    myPID_R.run();
+    
+    //DEBUG//
+    //Serial.print(true_speed_left);
+    //Serial.print(" ");
+    //Serial.println(true_speed_right);
+    
+    //Serial.print(String(sensor_left)+" ");
+    //Serial.print(String(sensor_middle)+" ");
+    //Serial.println(String(sensor_right)+" ");
+    
+    Serial.print(String(coef_left)+" ");
+    Serial.print(String(coef_right)+" ");
+    Serial.print(String(true_speed_left)+" ");
+    Serial.println(String(true_speed_right)+" ");
+  }
   update_motors_tension();
-  delay(300)
 }
 
 
-void update_measures(unsigned long delta_t){ // permet de connaitre la vitesse de rotation des moteurs
-  distance_step_left = WHEEL_RADIUS*2*PI*left_count/2940; //2940=7*2*210=pulses par rotation
-  distance_step_right = WHEEL_RADIUS*2*PI*right_count/2940; 
-  true_speed_left=distance_step_left*1000/delta_t;
-  true_speed_right=distance_step_right*1000/delta_t;
-  left_count=0;
-  right_count=0;
-  distance_step = (distance_step_left+distance_step_right)/2; // mesure la distance parcourue pendant delta t
-  distance_since_last_turn += distance_step; // mesure la distance total parcourue
+void update_measured_speeds(unsigned long delta_t){ // permet de connaitre la vitesse des moteurs
+  true_speed_left=get_distance(left_count-last_left_count)*1000/delta_t;
+  true_speed_right=get_distance(right_count-last_right_count)*1000/delta_t;
+  last_left_count = left_count;
+  last_right_count = right_count;
+}
+
+double get_distance(long count){ //renvoie la distance parcourure en fonction du nombre de pulses des encodeurs
+  return double(WHEEL_RADIUS*2*PI*count)/2940; //2940=7*2*210=pulses par rotation
 }
 
 void increment_left_count() {
@@ -136,16 +173,18 @@ void increment_left_count() {
 }
 
 void increment_right_count() {
-  if (digitalRead(ENCODER_PIN_A_R) != digitalRead(ENCODER_PIN_B_R))right_count--; // permet de connaitre le sens de rotation du moteur droit
-  else right_count++;
+  if (digitalRead(ENCODER_PIN_A_R) != digitalRead(ENCODER_PIN_B_R))right_count++; // permet de connaitre le sens de rotation du moteur droit
+  else right_count--;
 }
 
 void update_motors_tension(){
-  tension_left = update_tension(tension_left+delta_tension_left, 1); //delta_tension_left et right sont donnés par le PID
-  tension_right = update_tension(tension_right+delta_tension_right, 0);
+  coef_left+=delta_coef_left; //delta_coef_left et right sont donnés par le PID
+  coef_right+=delta_coef_right;
+  update_tension(speed_left*coef_left, 0);
+  update_tension(speed_right*coef_right, 1);
 }
 
-double update_tension(int tension, int motor //1 for left, 0 for right){
+double update_tension(int tension, int motor){//1 for left, 0 for right
   if (tension>255)tension = 255;
   if (tension<-255)tension = -255;
   boolean inPin1 = LOW;
